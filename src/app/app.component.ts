bf0723f958a341cdfe72aacf998b9474d002dfb7
Leandro Mayrink <32225957@ulife.com.br>
2023-11-06 10:13:40 -0300

    aula 06/11/23

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'curso-angular';
+  userName = 'Leandro';
+  userData = {
+    email: 'Mayrink@mail.com',
+    funcao: 'Admin'
+  }
+  title = 'curso-Angular';
 }
---
+++ src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-primeiro-componente',
+  templateUrl: './primeiro-componente.component.html',
+  styleUrls: ['./primeiro-componente.component.css']
+})
+export class PrimeiroComponenteComponent  implements OnInit {
+nome: string = 'Leandro';
+constructor() { }
+ngOnInit(): void { }
+}
